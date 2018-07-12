781a1be15e2fd5ecda6e876924a8d3c58f226661
Antônio Davi Macêdo Coêlho de Castro <adavimacedo@gmail.com>
2018-07-12 16:59:14 -0700

    Template class

---
+++ spec/lib/templates/Template.spec.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview Tests for lib/templates/template.js file.
+ */
+
+const Template = require('../../../lib/templates/Template');
+
+const template = new Template('FooCategory', 'FooTemplate');
+
+describe('lib/templates/Template.js', () => {
+  test('should export "Template" class', () => {
+    expect(template.constructor.name).toEqual('Template');
+  });
+
+  test('should fail if category or name is not a String', () => {
+    expect(() => new Template()).toThrowError(
+      'Could not initialize Template class: param "category" should be String'
+    );
+    expect(() => new Template('FooCategory')).toThrowError(
+      'Could not initialize Template class: param "name" should be String'
+    );
+    expect(() => new Template(null, 'FooTemplate')).toThrowError(
+      'Could not initialize Template class: param "category" should be String'
+    );
+  });
+
+  describe('Template.category', () => {
+    test('should be readonly', () => {
+      expect(template.category).toEqual('FooCategory');
+      template.category = 'AnotherFooCategory';
+      expect(template.category).toEqual('FooCategory');
+    });
+  });
+
+  describe('Template.name', () => {
+    test('should be readonly', () => {
+      expect(template.name).toEqual('FooTemplate');
+      template.name = 'AnotherFooTemplate';
+      expect(template.name).toEqual('FooTemplate');
+    });
+  });
+});
