34410dd6a267ba90331ee06680320f6f80a5e30c
Antônio Davi Macêdo Coêlho de Castro <adavimacedo@gmail.com>
2018-07-12 16:00:13 -0700

    Exposing Core plugin to be extended

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /**
- * Exports the {@link Ant} class that can be used to initialize the framework
- * and the {@link Plugin} class that can be used to extend the framework.
+ * Exports the {@link Ant} class that can be used to initialize the framework,
+ * the {@link Plugin} class that can be used to extend the framework and the
+ * default {@link Core} plugin that can be also extended.
  * @module antframework
  */
 
 const Ant = require('./lib/Ant');
 const Plugin = require('./lib/plugins/Plugin');
+const Core = require('./lib/plugins/core');
 
 module.exports.Ant = Ant;
 module.exports.Plugin = Plugin;
+module.exports.Core = Core;
