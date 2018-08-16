475b74a115339b690934fdb5ab3c0003333be19e
Douglas Muraoka <douglas.muraoka@gmail.com>
2018-08-16 20:15:16 -0300

    fix: Multiple $GLOBAL variables not being replaced (#40)

--- spec/lib/config/handler/VariablesResolver.spec.js
+++ spec/lib/config/handler/VariablesResolver.spec.js
@@ -37,4 +37,42 @@ describe('lib/config/handler/VariablesResolver.js', () => {
       test: 123
     });
   });
+
+  test('should replace multiple $GLOBAL var on JSON', () => {
+    const json = {
+      basePath: '$GLOBAL/foo/bar',
+      plugins: [
+        '$GLOBAL/my/plugins',
+        '$GLOBAL/my/plugins2',
+        {
+          '$GLOBAL/foo/bar1': {
+            basePath: '$GLOBAL/this/should/not/be/resolved',
+            '$GLOBAL/this/should/not/be/resolved': {}
+          }
+        },
+        {
+          '$GLOBAL/foo/bar2': {}
+        }
+      ],
+      test: 123
+    };
+    resolver.handle(json);
+    const expectedPlugin = {};
+    expectedPlugin[path.resolve(GLOBAL_DIR, 'foo/bar1')] = {
+      basePath: '$GLOBAL/this/should/not/be/resolved',
+      '$GLOBAL/this/should/not/be/resolved': {}
+    };
+    const expectedPlugin2 = {};
+    expectedPlugin2[path.resolve(GLOBAL_DIR, 'foo/bar2')] = {};
+    expect(json).toEqual({
+      basePath: path.resolve(GLOBAL_DIR, 'foo/bar'),
+      plugins: [
+        path.resolve(GLOBAL_DIR, 'my/plugins'),
+        path.resolve(GLOBAL_DIR, 'my/plugins2'),
+        expectedPlugin,
+        expectedPlugin2
+      ],
+      test: 123
+    });
+  });
 });
