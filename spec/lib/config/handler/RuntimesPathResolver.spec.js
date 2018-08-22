e33f556058691decee78f4ec9dc212bc51ee4c67
Douglas Muraoka <douglas.muraoka@gmail.com>
2018-08-22 19:53:10 -0300

    feat: User defined runtimes (#46)
    * feat: Function commands (add, remove, ls, exec)
    
    - Introducing functions basic commands (add, remove, ls, exec)
    - Allows user defined functions on configuration files
    
    * fix: function add command description
    
    * fix: improve error message when running LibFunction with unknown Runtime
    
    * fix: LibFunction breaks when handler returns undefined
    
    * fix: function add command breaks when adding LibFunction
    
    * refactor: functions as map on the configuration file
    
    * refactor: node runtime supports JSON as execution argument
    
    * feat: User defined runtimes on configuration files

---
+++ spec/lib/config/handler/RuntimesPathResolver.spec.js
@@ -0,0 +1,110 @@
+/**
+ * @fileoverview Tests for lib/config/handler/RuntimesPathResolver.js file.
+ */
+
+const RuntimesPathResolver = require('../../../../lib/config/handler/RuntimesPathResolver');
+const path = require('path');
+const resolver = new RuntimesPathResolver();
+
+const assertRuntimes = (runtimes, basePath, expectedRuntimes) => {
+  const json = basePath ? { basePath } : {};
+  json.runtimes = runtimes;
+  resolver.handle(json);
+  expect(json.runtimes).toEqual(expectedRuntimes);
+};
+
+describe('lib/config/handler/RuntimesPathResolver.js', () => {
+  test('should do nothing if does not contain runtimes', () => {
+    const json = {
+      basePath: '/foo/bar',
+      plugins: [
+        './my/plugin',
+        '/absolute/plugin'
+      ],
+      templates: {
+        MyCategory: {
+          MyTemplate: 'path/to/my/template',
+          AbsoluteTemplate: '/absolute/template'
+        }
+      },
+      functions: [
+        {
+          name: 'MyFunc',
+          bin: '/my/func'
+        },
+        {
+          name: 'MyLib',
+          handler: '/my/lib',
+          runtime: 'Default'
+        }
+      ]
+    };
+    resolver.handle(json);
+    expect(json).toEqual({
+      basePath: '/foo/bar',
+      plugins: [
+        './my/plugin',
+        '/absolute/plugin'
+      ],
+      templates: {
+        MyCategory: {
+          MyTemplate: 'path/to/my/template',
+          AbsoluteTemplate: '/absolute/template'
+        }
+      },
+      functions: [
+        {
+          name: 'MyFunc',
+          bin: '/my/func'
+        },
+        {
+          name: 'MyLib',
+          handler: '/my/lib',
+          runtime: 'Default'
+        }
+      ]
+    });
+  });
+
+  test('should do nothing if runtime does not have a bin', () => {
+    assertRuntimes(
+      { bla: { extensions: ['js'] }},
+      './foo/bar',
+      { bla: { extensions: ['js'] }}
+    );
+  });
+
+  describe('bin path', () => {
+    test('should resolve bin path with absolute function path and relative basePath', () => {
+      assertRuntimes(
+        { bla: { bin: '/absolute/my/function'}},
+        './foo/bar',
+        { bla: { bin: '/absolute/my/function'}}
+      );
+    });
+
+    test('should resolve bin path with absolute function path and absolute basePath', () => {
+      assertRuntimes(
+        { bla: { bin: '/absolute/my/function'}},
+        '/foo/bar',
+        { bla: { bin: '/absolute/my/function'}}
+      );
+    });
+
+    test('should resolve bin path with relative function path and relative basePath', () => {
+      assertRuntimes(
+        { bla: { bin: './my/function'}},
+        './foo/bar',
+        { bla: { bin: path.resolve(process.cwd(), './foo/bar/my/function')}}
+      );
+    });
+
+    test('should resolve bin path with relative function path and absolute basePath', () => {
+      assertRuntimes(
+        { bla: { bin: './my/function'}},
+        '/foo/bar',
+        { bla: { bin: '/foo/bar/my/function'}}
+      );
+    });
+  });
+});
