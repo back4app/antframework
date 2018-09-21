e7e10b7f94c13eeaee23720e9e64591abc511831
= <adavimacedo@gmail.com>
2018-09-20 17:28:25 -0700

    Fixing ant-util-docs and ant-util-tests packages module names

--- packages/ant-util-docs/index.js
+++ packages/ant-util-docs/index.js
@@ -1,6 +1,6 @@
 /**
  * This module does not export anything but it has utility configuration files
- * for leading with test tools in the context of
+ * for leading with docs in the context of
  * [Ant Framework]{@link https://github.com/back4app/antframework}.
- * @module antframework/packages/ant-util-tests
+ * @module ant-util-docs
  */
--- packages/ant-util-tests/index.js
+++ packages/ant-util-tests/index.js
@@ -2,5 +2,5 @@
  * This module does not export anything but it has utility configuration files
  * for leading with tests in the context of
  * [Ant Framework]{@link https://github.com/back4app/antframework}.
- * @module antframework/packages/ant-util-tests
+ * @module ant-util-tests
  */
